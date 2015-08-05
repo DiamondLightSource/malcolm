bd0e7e3c7f162af4dc54cf6d436433e86fa43d9c
Tom Cobb <tom.cobb@diamond.ac.uk>
2015-08-05 09:38:55 +0100

    Remove require statements

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,11 @@ def get_version():
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-from pkg_resources import require
-require("pyzmq")
+try:
+    from pkg_resources import require
+    require("pyzmq")
+except:
+    pass
 sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..', '..')))
 
 # -- General configuration ------------------------------------------------
